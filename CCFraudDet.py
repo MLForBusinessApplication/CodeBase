81233f7eae632503436a6a5141f25967bfdd7623
hrishihk24 <149948366+hrishihk24@users.noreply.github.com>
2023-12-08 08:02:28 -0500

    Add files via upload

--- CCFraudDet.py
+++ CCFraudDet.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel
 class CCFraud(BaseModel):
     variable14: float 
     variable12: float 
-    variable17: float 
+    variable4: float 
     Amount: float
--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 # 2. Create the app object
 app = FastAPI()
-pickle_in = open("classifier_nov11.pkl","rb")
+pickle_in = open("classifier_dec7.pkl","rb")
 classifier=pickle.load(pickle_in)
 
 
@@ -21,21 +21,21 @@ def predict_fraud(data:CCFraud):
     data = data.dict()
     variable14=data['variable14']
     variable12=data['variable12']
-    variable17=data['variable17']
+    variable4=data['variable4']
     Amount=data['Amount']
    
-    prediction = classifier.predict([[406,-2.312226542,1.951992011,-1.609850732,3.997905588,-0.522187865,-1.426545319,
+    prediction = classifier.predict([[406,-2.312226542,1.951992011,-1.609850732,variable4,-0.522187865,-1.426545319,
                                       -2.537387306, 1.391657248,-2.770089277,-2.772272145,3.202033207, variable12,
-                                      -0.595221881, variable14, 0.38972412, -1.14074718, variable17, -0.016822468,
+                                      -0.595221881, variable14, 0.38972412, -1.14074718, -2.83005567450437, -0.016822468,
                                       0.416955705, 0.126910559, 0.517232371, -0.035049369,-0.465211076,0.320198199,
                                       0.044519167, 0.177839798, 0.261145003, -0.143275875, Amount]])
     
     if(prediction[0]>0):
-        prediction="Fraud Transaction"
+        is_fraud = True
     else:
-        prediction="Not a Fraud Transaction"
+        is_fraud = False
     return {
-        'prediction': prediction
+        'Is it a Fraud Transaction?': is_fraud
     }
 
 # 5. Run the API with uvicorn
